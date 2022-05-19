7c1aea732883db4dc770d2af343ecca3a6496d47
matax123 <matias.ortiz@alumnos.ucn.cl>
2022-05-18 23:59:58 -0400

    feat: app header

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header >
+        <div className="HACKER-NEWS">
+          <span >
+            HACKER NEWS
+          </span>
+        </div>
+      </header>
+      <main>
+        <section>
+
+        </section>
+        <section className='news'>
+
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
