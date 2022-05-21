78216c1bbe6c1fad17a6b379d11c116f7ab52c47
matax123 <matias.ortiz@alumnos.ucn.cl>
2022-05-21 13:58:54 -0400

    feat: news filter load in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/App.css';
 import News from './components/News';
 
@@ -6,6 +6,7 @@ const App = () => {
 
   const [allNews, setAllNews] = useState(true);
   const [newsArray, setNewsArray] = useState([]);
+  const [filter, setFilter] = useState(localStorage.getItem('selectedNews') ? localStorage.getItem('selectedNews') : 'default');
 
   async function fetchNews(query: string) {
     let data: NewsResponseType[] = [];
@@ -26,6 +27,23 @@ const App = () => {
     return newNewsArray;
   }
 
+  useEffect(() => {
+    if (localStorage.getItem('selectedNews') && localStorage.getItem('selectedNews') !== 'default') {
+      fetchNews(localStorage.getItem('selectedNews')).then(res => {
+        setNewsArray(res);
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    if (filter !== 'default') {
+      console.log(filter)
+      fetchNews(filter).then(res => {
+        setNewsArray(res);
+      })
+    }
+  }, [filter])
+
   return (
     <div>
       <header className="app__header">
@@ -45,49 +63,49 @@ const App = () => {
           </div>
         </section>
         <section className='app__news-container'>
-            <div className='app__news-grid'>
-              <div className='app__select-news-container' style={
-                allNews ? { visibility: 'visible' } : { visibility: 'hidden' }
-              }>
-                <select onChange={async (e) => {
-                  if (e.target.value !== 'default') {
-                    await fetchNews(e.target.value).then(res => setNewsArray(res));
-                  }
+          <div className='app__news-grid'>
+            <div className='app__select-news-container' style={allNews ? { visibility: 'visible' } : { visibility: 'hidden' }}>
+              <select onChange={
+                async (e) => {
+                  localStorage.setItem('selectedNews', e.target.value);
+                  setFilter(e.target.value);
                 }
-                }>
-                  <option value="default">Select your news</option>
-                  <option value="Angular">Angular</option>
-                  <option value="React">React</option>
-                  <option value="Vue">Vue</option>
-                </select>
-              </div>
-
-              <div className='app__news'>
-                {allNews ?
-                  <div>
-                    {
-                      newsArray.map((news: NewsType) => {
-                        return <News
-                          {...news}
-                          key={news.id}
-                        />
-                      })
-                    }
-                  </div>
-                  :
-                  <div>
-                    {
-                      Object.keys(localStorage).map((key: string) => {
-                        return <News
-                          {...JSON.parse(localStorage.getItem(key))}
-                          key={key}
-                        />
-                      })
-                    }
-                  </div>
-                }
-              </div>
+              }
+              defaultValue={filter}
+              >
+                <option value="default" disabled hidden>Select your news</option>
+                <option value="Angular">Angular</option>
+                <option value="React">React</option>
+                <option value="Vue">Vue</option>
+              </select>
             </div>
+
+            <div className='app__news'>
+              {allNews ?
+                <div>
+                  {
+                    newsArray.map((news: NewsType) => {
+                      return <News
+                        {...news}
+                        key={news.id}
+                      />
+                    })
+                  }
+                </div>
+                :
+                <div>
+                  {
+                    Object.keys(localStorage).map((key: string) => {
+                      return <News
+                        {...JSON.parse(localStorage.getItem(key))}
+                        key={key}
+                      />
+                    })
+                  }
+                </div>
+              }
+            </div>
+          </div>
 
         </section>
       </main>
